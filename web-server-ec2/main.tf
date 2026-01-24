42c330c87973057b6863700c9f6b0cee0a86b5f8
Kevin González <keving5726@gmail.com>
2026-01-24 00:41:31 -0400

    Create main.tf file

---
+++ web-server-ec2/main.tf
@@ -0,0 +1,46 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+}
+
+resource "aws_instance" "web_server_ec2" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.instance_type
+  availability_zone      = var.availability_zone
+  vpc_security_group_ids = [aws_security_group.web_server_ec2_sg.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Congratulations, the web server is working successfully" > index.html
+              nohup busybox httpd -f -p ${var.server_port} &
+              EOF
+
+  user_data_replace_on_change = true
+
+  tags = {
+    Name = "Web Server EC2"
+  }
+}
+
+resource "aws_security_group" "web_server_ec2_sg" {
+  name        = var.sg_name
+  description = "Security group to allow traffic to the web server on port ${var.server_port}"
+
+  tags = {
+    Name = "Web Server EC2 SG"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ipv4" {
+  security_group_id = aws_security_group.web_server_ec2_sg.id
+
+  cidr_ipv4   = var.sg_cidr_ipv4
+  from_port   = var.server_port
+  ip_protocol = var.sg_ip_protocol
+  to_port     = var.server_port
+}
