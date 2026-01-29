27a6a147384e2965e8cd1ac9ca9f2761ba7c2113
Kevin González <keving5726@gmail.com>
2026-01-28 23:53:20 -0400

    Update variables.tf file

--- web-server-cluster/variables.tf
+++ web-server-cluster/variables.tf
@@ -3,3 +3,51 @@ variable "aws_region" {
   type        = string
   default     = "us-east-1"
 }
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.nano"
+}
+
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 8080
+}
+
+variable "alb_port" {
+  description = "The port the ALB will use for HTTP requests"
+  type        = number
+  default     = 80
+}
+
+variable "protocol" {
+  description = "Protocol to use for routing traffic"
+  type        = string
+  default     = "HTTP"
+}
+
+variable "instance_sg_name" {
+  description = "The name of the security group of the instances"
+  type        = string
+  default     = "web-server-sg"
+}
+
+variable "alb_sg_name" {
+  description = "The name of the security group of the Application Load Balancer"
+  type        = string
+  default     = "alb-sg"
+}
+
+variable "ip_protocol" {
+  description = "The IP protocol name of the security group"
+  type        = string
+  default     = "tcp"
+}
+
+variable "cidr_ipv4" {
+  description = "The source IPv4 CIDR range of the security group"
+  type        = string
+  default     = "0.0.0.0/0"
+}
