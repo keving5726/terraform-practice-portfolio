e3c45b6a12c2f9ad883bbab2b746e4c4b8d66579
Kevin González <keving5726@gmail.com>
2026-02-06 20:18:01 -0400

    Delete 'availability_zone' variable

--- web-server-ec2/variables.tf
+++ web-server-ec2/variables.tf
@@ -4,12 +4,6 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
-variable "availability_zone" {
-  description = "Availability zone of the instance"
-  type        = string
-  default     = "us-east-1a"
-}
-
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
