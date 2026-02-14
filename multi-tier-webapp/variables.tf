24b447b98e13c5802870c7e2db36394ee81dca3d
Kevin González <keving5726@gmail.com>
2026-02-14 00:09:49 -0400

    Delete instance type

--- multi-tier-webapp/variables.tf
+++ multi-tier-webapp/variables.tf
@@ -8,9 +8,3 @@ variable "aws_region" {
   type        = string
   default     = "us-east-1"
 }
-
-variable "instance_type" {
-  description = "EC2 instance type"
-  type        = string
-  default     = "t2.nano"
-}
