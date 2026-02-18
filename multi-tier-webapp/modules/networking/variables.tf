29358622c7a8cd09021a3ef43359ede36ddba12b
Kevin González <keving5726@gmail.com>
2026-02-17 22:30:38 -0400

    Add ports

--- multi-tier-webapp/modules/networking/variables.tf
+++ multi-tier-webapp/modules/networking/variables.tf
@@ -19,3 +19,15 @@ variable "database_subnets" {
   type        = list(string)
   default     = ["10.0.21.0/24", "10.0.22.0/24", "10.0.23.0/24"]
 }
+
+variable "web_server_port" {
+  description = "The port the web server will use for HTTP requests"
+  type        = number
+  default     = 8080
+}
+
+variable "alb_port" {
+  description = "The port the ALB will use for HTTP requests"
+  type        = number
+  default     = 80
+}
