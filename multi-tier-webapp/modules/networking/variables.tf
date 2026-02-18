224dd73feb36b9228ec43004a53ae2c00655c712
Kevin González <keving5726@gmail.com>
2026-02-17 22:30:04 -0400

    Add subnets

--- multi-tier-webapp/modules/networking/variables.tf
+++ multi-tier-webapp/modules/networking/variables.tf
@@ -1,3 +1,21 @@
 variable "namespace" {
   type        = string
 }
+
+variable "private_subnets" {
+  description = "List of private subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "public_subnets" {
+  description = "List of public subnets"
+  type        = list(string)
+  default     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+}
+
+variable "database_subnets" {
+  description = "List of database subnets"
+  type        = list(string)
+  default     = ["10.0.21.0/24", "10.0.22.0/24", "10.0.23.0/24"]
+}
