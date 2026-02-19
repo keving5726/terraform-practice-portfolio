23499aef89f000b2bc3248791463f93dd26331d6
Kevin González <keving5726@gmail.com>
2026-02-18 23:56:01 -0400

    Add ALB security group

--- multi-tier-webapp/modules/networking/main.tf
+++ multi-tier-webapp/modules/networking/main.tf
@@ -25,3 +25,18 @@ module "vpc" {
     Environment = "dev"
   }
 }
+
+module "alb_sg" {
+  source  = "terraform-aws-modules/security-group/aws//modules/http-80"
+  version = "5.3.1"
+
+  name        = "alb-sg"
+  description = "Security group to allow traffic to the ALB on port ${var.alb_port}"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress_cidr_blocks = var.allow_all_cidr
+
+  tags = {
+    Name = "Application Load Balancer SG"
+  }
+}
