802f51708cc3bc834061042cf8eef0518efb34ff
Kevin González <keving5726@gmail.com>
2026-02-21 23:07:00 -0400

    Create outputs.tf file

---
+++ multi-tier-webapp/modules/database/outputs.tf
@@ -0,0 +1,9 @@
+output "db_config" {
+  value = {
+    hostname = aws_db_instance.database.address
+    port     = aws_db_instance.database.port
+    database = aws_db_instance.database.db_name
+    user     = aws_db_instance.database.username
+    password = aws_db_instance.database.password
+  }
+}
