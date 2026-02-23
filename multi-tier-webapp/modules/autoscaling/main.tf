c7c0c0978ffce2efc4a57aeba0f09fa5d2af7ae5
Kevin González <keving5726@gmail.com>
2026-02-23 18:25:33 -0400

    Add AWS IAM Role module

--- multi-tier-webapp/modules/autoscaling/main.tf
+++ multi-tier-webapp/modules/autoscaling/main.tf
@@ -18,3 +18,36 @@ data "aws_ami" "ubuntu" {
     values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
   }
 }
+
+module "iam_role_instance_profile" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role"
+  version = "6.4.0"
+
+  name                    = "${var.namespace}-instance-profile"
+  use_name_prefix         = false
+  description             = "Allows RDS to perform operations using AWS resources on your behalf and allows CloudWatch to manage EC2 instances on your behalf"
+  create_instance_profile = true
+
+  trust_policy_permissions = {
+    ec2 = {
+      effect = "Allow"
+      actions = [
+        "sts:AssumeRole"
+      ]
+      principals = [{
+        type        = "Service"
+        identifiers = ["ec2.amazonaws.com"]
+      }]
+    }
+  }
+
+  policies = {
+    CloudWatchLogsFullAccess = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
+    AmazonRDSFullAccess      = "arn:aws:iam::aws:policy/AmazonRDSFullAccess"
+  }
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
