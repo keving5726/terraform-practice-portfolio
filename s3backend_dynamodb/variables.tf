9b7464bce653534292363c1ffcf19b041a6bd826
Kevin González <keving5726@gmail.com>
2026-03-09 22:24:45 -0400

    Add force destroy state

--- s3backend_dynamodb/variables.tf
+++ s3backend_dynamodb/variables.tf
@@ -9,3 +9,9 @@ variable "principal_arns" {
   default     = null
   type        = list(string)
 }
+
+variable "force_destroy_state" {
+  description = "Force destroy the S3 bucket containing state files"
+  default     = true
+  type        = bool
+}
