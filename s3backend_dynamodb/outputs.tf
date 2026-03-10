a601dbef71a9c8747340490ba9fdf58ebc121be2
Kevin González <keving5726@gmail.com>
2026-03-10 15:41:10 -0400

    Fix values in outputs.tf file

--- s3backend_dynamodb/outputs.tf
+++ s3backend_dynamodb/outputs.tf
@@ -1,9 +1,9 @@
 output "config" {
   description = "S3 backend configuration"
   value = {
-    bucket         = aws_s3_bucket.s3_bucket.bucket
+    bucket         = aws_s3_bucket.tf_backend.bucket
     region         = data.aws_region.current.region
-    role_arn       = aws_iam_role.iam_role.arn
-    dynamodb_table = aws_dynamodb_table.dynamodb_table.name
+    role_arn       = aws_iam_role.tf_backend.arn
+    dynamodb_table = aws_dynamodb_table.tf_backend.name
   }
 }
