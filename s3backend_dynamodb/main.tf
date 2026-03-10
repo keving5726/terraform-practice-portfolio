471d5aada3217708e42e46586e3649f500a0ef9a
Kevin González <keving5726@gmail.com>
2026-03-10 10:49:57 -0400

    Add resource group

--- s3backend_dynamodb/main.tf
+++ s3backend_dynamodb/main.tf
@@ -9,3 +9,24 @@ resource "random_string" "rand" {
 locals {
   namespace = substr(join("-", [var.namespace, random_string.rand.result]), 0, 24)
 }
+
+resource "aws_resourcegroups_group" "tf_backend" {
+  name        = "${local.namespace}-group"
+  description = "Terraform resource group for S3 backend"
+
+  resource_query {
+    query = <<-JSON
+{
+  "ResourceTypeFilters": [
+    "AWS::AllSupported"
+  ],
+  "TagFilters": [
+    {
+      "Key": "ResourceGroup",
+      "Values": ["${local.namespace}"]
+    }
+  ]
+}
+  JSON
+  }
+}
