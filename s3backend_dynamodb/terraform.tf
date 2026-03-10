815c6cc73fb35d10870ccc6207c47aed47ac1990
Kevin González <keving5726@gmail.com>
2026-03-09 22:21:30 -0400

    Create terraform.tf file

---
+++ s3backend_dynamodb/terraform.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = "~> 1.14.5"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.33.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.8.1"
+    }
+  }
+}
