7c373ef6c5d13da799486593fb99815c235995ee
Kevin González <keving5726@gmail.com>
2026-03-10 11:12:39 -0400

    Add policy document

--- s3backend_dynamodb/iam.tf
+++ s3backend_dynamodb/iam.tf
@@ -1,1 +1,37 @@
 data "aws_caller_identity" "current" {}
+
+data "aws_iam_policy_document" "tf_backend" {
+  statement {
+    actions = [
+      "s3:ListBucket",
+    ]
+
+    resources = [
+      aws_s3_bucket.s3_bucket.arn,
+    ]
+  }
+
+  statement {
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject",
+      "s3:DeleteObject",
+    ]
+
+    resources = [
+      "${aws_s3_bucket.s3_bucket.arn}/*",
+    ]
+  }
+
+  statement {
+    actions = [
+      "dynamodb:GetItem",
+      "dynamodb:PutItem",
+      "dynamodb:DeleteItem",
+    ]
+
+    resources = [
+      aws_dynamodb_table.dynamodb_table.arn,
+    ]
+  }
+}
