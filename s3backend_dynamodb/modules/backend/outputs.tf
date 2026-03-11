21141683cd319bcb72502836571a87291e2325d7
Kevin González <keving5726@gmail.com>
2026-03-11 19:30:20 -0400

    Sort configuration

--- s3backend_dynamodb/modules/backend/outputs.tf
+++ s3backend_dynamodb/modules/backend/outputs.tf
@@ -3,7 +3,7 @@ output "config" {
   value = {
     bucket         = aws_s3_bucket.tf_backend.bucket
     region         = data.aws_region.current.region
-    role_arn       = aws_iam_role.tf_backend.arn
     dynamodb_table = aws_dynamodb_table.tf_backend.name
+    role_arn       = aws_iam_role.tf_backend.arn
   }
 }
