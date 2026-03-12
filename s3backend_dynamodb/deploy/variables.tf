0588e669abd72165533c9eae6031c6c968568a7e
Kevin González <keving5726@gmail.com>
2026-03-11 20:09:04 -0400

    Add deploy for the S3 backend

---
+++ s3backend_dynamodb/deploy/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  description = "AWS Region where the instance will be deployed"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "namespace" {
+  description = "The project namespace to use for unique resource naming"
+  type        = string
+  default     = "s3backend-dynamodb"
+}
