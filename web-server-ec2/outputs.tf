f946fcb21ef6f073a94fe113e4404fb88ab065e6
Kevin González <keving5726@gmail.com>
2026-03-25 09:29:47 -0400

    Fix output block formatting

--- web-server-ec2/outputs.tf
+++ web-server-ec2/outputs.tf
@@ -1,4 +1,4 @@
 output "public_ip" {
-  value       = aws_instance.web_server_ec2.public_ip
   description = "The public IP address of the web server"
+  value       = aws_instance.web_server_ec2.public_ip
 }
